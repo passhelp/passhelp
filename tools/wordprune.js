242d5660390302068ab50d376906410a6a311f3c
Jacob Peddicord <jacob@peddicord.net>
2016-09-11 09:28:29 -0700

    Pruned word list a bit more
    Still above 10k

--- tools/wordprune.js
+++ tools/wordprune.js
@@ -1,28 +1,31 @@
-const readline = require('readline');
+if (process.argv.length !== 4) {
+  console.log('usage: wordprune words.txt exclusions.txt');
+  process.exit(1);
+}
 
-const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
+const fs = require('fs');
+
+const source = fs.readFileSync(process.argv[2], {encoding: 'utf8'});
+const excludeSource = fs.readFileSync(process.argv[3], {encoding: 'utf8'});
+
+const exclude = excludeSource.split('\n');
 
 // when combined with 3esl from 12dicts, this should yield ~10K words
 // excludes hyphenations, multi-word phrases, abbreviations, acronyms, proper nouns
 const valid = /^[a-z]{5,8}$/;
 
 const words = [];
-
-reader.on('line', line => {
+source.split('\n').forEach(line => {
   const trimmed = line.trim();
   if (valid.test(trimmed)) {
-    words.push(trimmed);
+    if (!exclude.includes(trimmed)) {
+      words.push(trimmed);
+    }
   }
 });
 
 // separate with a comma instead of a newline:
 // when bundled, webpack will write '\n' for newlines (as it should).
 // but why waste the space?
-reader.on('close', () => {
-  process.stdout.write(words.join(','));
-});
-
+process.stdout.write(words.join(','));
+process.stderr.write(`generated ${words.length} words\n`);

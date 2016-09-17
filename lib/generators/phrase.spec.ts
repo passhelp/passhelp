786bb050aa8ec3670f8b0083275b996627dccaf6
Jacob Peddicord <jacob@peddicord.net>
2016-09-17 15:07:06 -0700

    Add phrase tests

---
+++ lib/generators/phrase.spec.ts
@@ -0,0 +1,16 @@
+import * as phrase from './phrase';
+
+describe('phrase generator', () => {
+
+  it('generates phrases', () => {
+    const pass = phrase.generate(5);
+    expect(pass.split(' ').length).toBe(5);
+  });
+
+  it('uses special characters as separators when requested', () => {
+    const pass = phrase.generate(5, true);
+    expect(pass).toMatch(/^\S+$/);
+    expect(pass).toMatch(/(\W|\d)/);
+  });
+
+});
